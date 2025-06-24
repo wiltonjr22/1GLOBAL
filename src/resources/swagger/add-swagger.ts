31008c9a1ad794ef5cc8eb9eccf280755489b326
wilton <wiltonsousajr@hotmail.com>
2025-06-24 20:45:38 -0300

    hotfix: fixing to map and execute the routes

--- src/resources/swagger/add-swagger.ts
+++ src/resources/swagger/add-swagger.ts
@@ -1,25 +1,17 @@
-import { ContextsModule } from "@/contexts/contexts.module";
 import { INestApplication } from "@nestjs/common";
-import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 export default function AddSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle("1GLOBAL API")
     .setDescription("API device")
-    .setVersion("0.0.1")
+    .setVersion("0.0.1");
 
   if (process.env.NODE_ENV !== "production") {
     config.addTag("Development Environment");
   }
 
-  const options: SwaggerDocumentOptions = {
-    include: [
-      ContextsModule,
-    ],
-  };
-
-  const document = SwaggerModule.createDocument(app, config.build(), options);
-
+  const document = SwaggerModule.createDocument(app, config.build());
 
   SwaggerModule.setup("swagger", app, document);
 }
