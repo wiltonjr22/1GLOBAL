5e820805e85b40031f9066c8040a0d91cc7bf42f
wilton <wiltonsousajr@hotmail.com>
2025-06-25 15:22:43 -0300

    doc: my opnion about the Product

--- src/contexts/device/application/services/device.service.ts
+++ src/contexts/device/application/services/device.service.ts
@@ -7,14 +7,17 @@ import { IDeviceRepository } from "../../infra/interfaces/device.repository";
 
 @Injectable()
 export class DeviceService implements IDeviceService {
-  constructor(private readonly deviceRepository: IDeviceRepository) { }
+  constructor(private readonly deviceRepository: IDeviceRepository) {
+    // put logs to information of service
+  }
 
   async create(createDeviceDto: CreateDeviceDto): Promise<void> {
-    this.deviceRepository.create(createDeviceDto);
+    await this.deviceRepository.create(createDeviceDto);
   }
 
   async findAll(): Promise<DeviceEntity[]> {
-    return this.deviceRepository.findAll();
+    return await this.deviceRepository.findAll();
+    // limit the number of devices returned (possible problem with too many devices)
   }
 
   async findOne(id: number): Promise<DeviceEntity> {
@@ -24,11 +27,13 @@ export class DeviceService implements IDeviceService {
   }
 
   async findByBrand(brand: string): Promise<DeviceEntity[]> {
-    return this.deviceRepository.findByBrand(brand);
+    return await this.deviceRepository.findByBrand(brand);
+    // limit the number of devices returned (possible problem with too many devices)
   }
 
   async findByState(state: DeviceState): Promise<DeviceEntity[]> {
-    return this.deviceRepository.findByState(state);
+    return await this.deviceRepository.findByState(state);
+    // limit the number of devices returned (possible problem with too many devices)
   }
 
   async update(id: number, updateDto: UpdateDeviceDto): Promise<DeviceEntity> {
@@ -41,10 +46,11 @@ export class DeviceService implements IDeviceService {
       throw new BadRequestException("Name and brand cannot be updated if device is in use");
     }
 
-    return this.deviceRepository.update(id, updateDto);
+    return await this.deviceRepository.update(id, updateDto);
   }
 
   async remove(id: number): Promise<void> {
+    // better to keep the record and just change the state from available to unavailable or perhaps create another column in the table to manage devices that have been asked to be deleted but still keep the data for BI decision-making.
     const device = await this.findOne(id);
     if (device.state === DeviceState.IN_USE) {
       throw new BadRequestException("In use devices cannot be deleted");
@@ -52,3 +58,4 @@ export class DeviceService implements IDeviceService {
     await this.deviceRepository.delete(id);
   }
 }
+
